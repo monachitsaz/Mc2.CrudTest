44e051ca58e118c8d5a55c5fe07080647b44a91b
monachitsaz <117484648+monachitsaz@users.noreply.github.com>
2023-01-14 03:52:00 +0330

    create customer entity and add to dbset

--- Mc2.CrudTest.Presentation/Server/Infra/CrudTestDbContext.cs
+++ Mc2.CrudTest.Presentation/Server/Infra/CrudTestDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mc2.CrudTest.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,24 @@ namespace Mc2.CrudTest.Presentation.Server.Infra
         public CrudTestDbContext(DbContextOptions<CrudTestDbContext> options)
            : base(options)
         {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasIndex(c => new { c.FirstName, c.LastName, c.DateOfBirth }).IsUnique();
+                entity.HasIndex(c => c.Email).IsUnique();
+                entity.Property(x => x.BankAccountNumber).HasMaxLength(20);
+                entity.Property(c => c.Email).HasMaxLength(25);
+                entity.Property(c => c.FirstName).HasMaxLength(25);
+                entity.Property(c => c.LastName).HasMaxLength(25);
+
+            });
+            base.OnModelCreating(modelBuilder);
+
         }
     }
 }
---
+++ Mc2.CrudTest.Presentation/Shared/Domain/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Shared.Domain
+{
+    public class Customer
+    {
+       
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        [NotMapped]
+        public string PhoneNumber { get; set; }
+
+        public int CountryCode { get; set; }
+
+        public ulong NationalNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string BankAccountNumber { get; set; }
+
+       
+    }
+}
