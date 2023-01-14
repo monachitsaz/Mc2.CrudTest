cd3db85923c11fbe2d97b551f4c554667e5befb5
monachitsaz <117484648+monachitsaz@users.noreply.github.com>
2023-01-14 03:43:25 +0330

    add dbcontext and connection string to startup and appjson

--- Mc2.CrudTest.Presentation/Server/Startup.cs
+++ Mc2.CrudTest.Presentation/Server/Startup.cs
@@ -1,5 +1,7 @@
+using Mc2.CrudTest.Presentation.Server.Infra;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,8 @@ namespace Mc2.CrudTest.Presentation.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddDbContext<CrudTestDbContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
