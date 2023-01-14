cba06acde6b0913655e6fe7c506f4abdd0d2e95b
monachitsaz <117484648+monachitsaz@users.noreply.github.com>
2023-01-14 03:40:11 +0330

    create dbcontext

---
+++ Mc2.CrudTest.Presentation/Server/Infra/CrudTestDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Presentation.Server.Infra
+{
+    public class CrudTestDbContext : DbContext
+    {
+
+        public CrudTestDbContext(DbContextOptions<CrudTestDbContext> options)
+           : base(options)
+        {
+        }
+    }
+}
